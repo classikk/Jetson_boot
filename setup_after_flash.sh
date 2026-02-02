52424decb92528b84b55f119eba55e93d300571d
Lassi <lassi.kokkonen@aalto.fi>
2026-02-02 20:53:48 +0200

    clone repo ssh

--- setup_after_flash.sh
+++ setup_after_flash.sh
@@ -8,6 +8,11 @@ sudo apt-get install ./vscode.deb
 rm ./vscode.deb
 
 ssh-keygen -t ed25519
-
 echo "copy this to git for ssh git pushes: "
 cat ~/.ssh/id_ed25519.pub
+
+read -p "Using firefox add public key to ssh keys, once done press Enter to continue"
+echo "testing clone with ssh"
+git clone git@github.com:CatScanners/find-my-kitten.git
+cd ./find-my-kitten
+echo "repo has been cloned"
