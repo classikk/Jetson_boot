032bcd3a319c2288f9c9efdf844fbf3ff7739e85
classik <lassi04kokkonen@gmail.com>
2026-02-17 16:50:47 +0200

    more commands

--- setup_after_flash.sh
+++ setup_after_flash.sh
@@ -32,14 +32,74 @@ echo "Git global config updated."
 #cat /etc/nvpmodel.conf
 #sudo nvpmodel -m <insert max>
 
+#git clone git@github.com:classikk/jetson_benchmark.git
+#cd ./jetson_benchmark
+
 sudo apt install python3
 pip install uv
 uv sync
 
+git submodule update --init --recursive
+
 sudo apt-get install tmux
 
-./simulation/setup.sh
 
-git clone git@github.com:classikk/jetson_benchmark.git
-cd ./jetson_benchmark
+# FROM https://nvidia-isaac-ros.github.io/v/release-3.1/getting_started/dev_env_setup.html
+sudo systemctl daemon-reload && sudo systemctl restart docker
 
+sudo apt-get install git-lfs
+git lfs install --skip-repo
+
+mkdir -p  ~/workspaces/isaac_ros-dev/src
+echo "export ISAAC_ROS_WS=${HOME}/workspaces/isaac_ros-dev/" >> ~/.bashrc
+source ~/.bashrc
+
+
+#https://nvidia-isaac-ros.github.io/v/release-3.1/getting_started/isaac_apt_repository.html
+locale
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale
+
+sudo apt update && sudo apt install gnupg wget
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+wget -qO - https://isaac.download.nvidia.com/isaac-ros/repos.key | sudo apt-key add -
+grep -qxF "deb https://isaac.download.nvidia.com/isaac-ros/release-3 $(lsb_release -cs) legacy-release-3.1" /etc/apt/sources.list || \
+echo "deb https://isaac.download.nvidia.com/isaac-ros/release-3 $(lsb_release -cs) legacy-release-3.1" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+#Install core ROS 2 Packages
+sudo apt update
+
+sudo apt install ros-humble-desktop-full
+
+sudo apt install ros-dev-tools
+
+
+curl -sSL https://get.docker.com/ | sh
+
+sudo usermod -aG docker $USER && newgrp docker
+
+git lfs fetch --all
+
+git lfs checkout
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+
+sudo nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
+
+ros2_ws/start_isaac_dev.sh
