f08af315714fc7770344d978b5bec1623447e1bb
Lassi <lassi.kokkonen@aalto.fi>
2026-02-19 22:41:09 +0200

    lanching firefox

--- setup_after_flash.sh
+++ setup_after_flash.sh
@@ -23,6 +23,7 @@ ssh-keygen -t ed25519
 echo "copy this to git for ssh git pushes: "
 cat ~/.ssh/id_ed25519.pub
 
+firefox
 read -p "Using firefox add public key to ssh keys, once done press Enter to continue"
 echo "testing clone with ssh"
 git clone git@github.com:CatScanners/find-my-kitten.git
